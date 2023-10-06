048e1054b7c0a8784d2480d8b590e19369ac3eaa
apella1 <johncollons5@gmail.com>
2023-10-06 09:15:47 +0300

    Solve roman to integer

---
+++ python_leetcode/easy/roman_to_integer.py
@@ -0,0 +1,17 @@
+"""Roman to integer"""
+
+
+def roman_to_integer(text: str) -> int:
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+
+    for letter in range(len(text)):
+        if letter + 1 < len(text) and roman[text[letter]] < roman[text[letter + 1]]:
+            result -= roman[text[letter]]
+        else:
+            result += roman[text[letter]]
+
+    return result
+
+
+print(roman_to_integer("IX"))
