7ed195a5155ef2c13715dfde4482ea782dc1f263
apella1 <johncollons5@gmail.com>
2024-01-12 22:54:57 +0300

    add contains duplicate hashset solution

--- python_leetcode/easy/contains_duplicate.py
+++ python_leetcode/easy/contains_duplicate.py
@@ -1,8 +1,10 @@
 """NeetCode 150: Arrays and Hashing"""
 
 
+# time -> O(n^2)
+# space -> O(1)
 def contains_duplicate(nums: list[int]) -> bool:
-    for i in range(len(nums)):
+    for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
             if nums[i] == nums[j]:
                 return True
@@ -17,4 +19,15 @@ def contains_duplicate_hashmap(nums: list[int]) -> bool:
         nums_map[num] += 1
         if nums_map[num] > 1:
             return True
+    return False
+
+
+# time -> O(n)
+# space -> O(n)
+def contains_duplicate_hashset(nums: list[int]) -> bool:
+    hash_set = set()
+    for n in nums:
+        if n in hash_set:
+            return True
+        hash_set.add(n)
     return False
